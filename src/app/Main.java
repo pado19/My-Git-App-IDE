32bfe511503f910cba528404b325df987ee102a4
pado19 <pavel.dobrovolskyi@gmail.com>
2023-08-25 21:16:08 +0100

    Update app info

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,6 +3,6 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My git app.");
+        System.out.println("This is shop-online app.");
     }
 }
